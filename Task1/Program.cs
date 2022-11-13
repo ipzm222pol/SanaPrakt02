0bfce3733d8dec20f64c8f4bf276ee854f9e3bed
Oleksandr Parkhomenko <ipzm222_pol@student.ztu.edu.ua>
2022-11-13 11:54:42 +0200

    3 first lines have been changed in output message

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Super!");
+Console.WriteLine("Курси Java also cool language");
+Console.WriteLine("Курси Not only .Net");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
